3b7a02577e1f52ad9b22fa86895d15909140b1ed
TrainerADP016 <arnob15-375@diu.edu.bd>
2023-03-23 22:51:38 +0600

    day5 update for 30daysofJS and100daysofCode challa

---
+++ 30DaysOfJS/day5.js
@@ -0,0 +1,32 @@
+for(let i = 1; i <=5; i++){
+    console.log("Day "+i+" Of JS")
+}
+
+let i = 1;
+
+while(i<=5){
+    console.log("Day "+i+" Of Code")
+    i++;
+}
+console.log('Value of i:',i);
+
+do{
+    if(i%2==0) console.log('But I can print ', i);
+    i++;
+} while (i<=5)
+
+let MySelf = {
+    name : 'Arnob',
+    age : 23,
+    occupation : 'Student',
+    mood: 'bored',
+    about: 'Procrastination'
+};
+let FavFood = ['Egg', 'Chocolate', 'Milk']
+
+for(let key in MySelf){
+    console.log(key, ':', MySelf.key);
+}
+for(let index in FavFood){
+    console.log(index, FavFood[index]);
+}
