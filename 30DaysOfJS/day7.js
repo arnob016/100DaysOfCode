2337e4f804a27a2ca81604b610784264848d4456
TrainerADP016 <arnob15-375@diu.edu.bd>
2023-03-26 19:08:54 +0600

    day 8 upload

--- 30DaysOfJS/day7.js
+++ 30DaysOfJS/day7.js
@@ -1,30 +1,25 @@
-//Excercise : Create a function that prints the prime numbers upto n;
-
-let n = 100;
-console.log('Printing prime numbers upto ' + n);
+let n = 1000;
+console.log("Printing prime numbers upto " + n);
 console.log(2);
-
 primeFunc(n);
-function primeFunc(num){
 
-    for (let n = 2; n <= num; n++){
-        let flag = true;
-        
-        for(let i=2; i<n; i++)
-        {
-            if (n%i === 0)
-            {
-                flag = true;
-                break;
-            }
-            else
-            {
-                flag = false;
-            }
-        }
-        if(flag === false)
-        {
-            console.log(n);
-        }
+function primeFunc(num) {
+  let k=0;
+  for (let n = 2; n <= num; n++) {
+    let flag = true;
+
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        flag = true;
+        break;
+      } else {
+        flag = false;
+      }
     }
+    if (flag === false) {
+      console.log(n);
+      k++;
+    }
+  }
+  console.log("Total number of primes:", k)
 }
